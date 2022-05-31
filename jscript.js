e44ab6a70d75ece962621ca9ded37de8c9c7ccf2
Treyce Wheeler <t.zeryon@gmail.com>
2022-05-30 22:08:21 -0400

    Start skeleton

---
+++ jscript.js
@@ -0,0 +1,40 @@
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+// function operate () {
+//     // let operator = prompt('Enter operator');
+//     // let num1 = prompt('Enter first number');
+//     // let num2 = prompt('Enter second number');
+
+//     if (operator == '+') {
+//         add(num1, num2);
+//     }
+
+//     else if (operator == '-') {
+//         subtract(num1, num2);
+//     }
+
+//     else if (operator == '*') {
+//         multiply(num1, num2);
+//     }
+
+//     else if (operator == '/') {
+//         divide(num1, num2);
+//     }
+// }
+
+// operate();
